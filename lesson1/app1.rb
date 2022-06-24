f6782c86a9d41849cceecc5da94d4daf9b70190d
Kirill <kirill.bezik@gmail.com>
2022-06-25 01:48:40 +0300

    Первые замечания

--- lesson1/app1.rb
+++ lesson1/app1.rb
@@ -3,8 +3,8 @@ name = gets.strip.capitalize
 puts "А какой твой рост? "
 height = gets.to_i
 ideal_weight = ((height - 110) * 1.15).to_f
-    if  ideal_weight < 0
+if  ideal_weight < 0
 	puts "#{name}, твой вес уже идеален!"
 	else
 	puts "#{name}, твой идеальный вес #{ideal_weight.round(2)}, задумайся!"
-    end
+end
--- lesson1/app4.rb
+++ lesson1/app4.rb
@@ -8,14 +8,14 @@ c = gets.to_f
 #Вычисляем дискриминант
 d = b**2 - 4*a*c
 #Проверяем дискриминант и вычисляем квадратные корни
-	if d < 0
-		puts "Корней нет!" 
+if d < 0
+	puts "Корней нет!" 
 	elsif d > 0
-		x1 = (-b + Math.sqrt(d)) / (2 * a)  
-		x2 = (-b - Math.sqrt(d)) / (2 * a)  
-		puts "Первый корень равен: #{x1.to_f.round(2)}"	
-		puts "Второй корень равен: #{x2.to_f.round(2)}"
+	x1 = (-b + Math.sqrt(d)) / (2 * a)  
+	x2 = (-b - Math.sqrt(d)) / (2 * a)  
+	puts "Первый корень равен: #{x1.to_f.round(2)}"	
+	puts "Второй корень равен: #{x2.to_f.round(2)}"
 	else
-		x1 = -b / (2 * a)
-		puts "Корень равен: #{x1.to_f.round(2)}"	
-	end	
+	x1 = -b / (2 * a)
+	puts "Корень равен: #{x1.to_f.round(2)}"	
+end	
